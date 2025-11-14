b884f1f4344b3dfe9239da58270bc525a1e88fda
aryhsg <62255390+aryhsg@users.noreply.github.com>
2025-11-14 10:47:20 +0800

    Implement scrape_all_news endpoint
    增加一個API端點可以一次抓所有類別的新聞

--- api.py
+++ api.py
@@ -40,6 +40,35 @@ async def scrape_news(category: str = Query(..., description="新聞分類"),
 
         raise HTTPException(status_code=500, detail=str(e))
 
+@app.get("/api/scrape-all-news/")
+async def scrape_all_news(crawler: NewsCrawler = Depends(get_news_crawler)):
+    category_list = ["要聞", "產業", "證券", "國際", "金融", "期貨", "理財", "房市", "專欄", "專題", "商情", "兩岸"]
+    all_news_list = []
+    
+    try:
+        for category in category_list:
+            news_category = News(source_type=category)
+            crawler.generate_URLs(news_category.source_url)
+            crawler.news_crawler()
+
+            urllist = crawler.url_list
+            titlelist = crawler.title_list
+            contents = crawler.content_list
+
+            picked_news = {
+                    "category": category,
+                    "url": urllist,
+                    "title": titlelist,
+                    "content": contents
+            }
+            all_news_list.append(picked_news)
+        return {"status": "success", "data": all_news_list}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error in category {category}: {str(e)}")
+
+
 @app.get("/")
 def read_root():
     return {"status": "OK"}
+
